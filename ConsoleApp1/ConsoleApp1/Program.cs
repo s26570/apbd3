a9826f426b273744d3f8f271dda25abf4a4fa1d1
Alicja Kochańska <s26570@pjwstk.edu.pl>
2024-03-18 09:34:50 +0100

    Utworzenie ciala metody

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,13 @@
 
     static double srednia(int[] tab)
     {
-        
+        int suma = 0;
+        for(int i = 0; i < tab.Length; i++)
+        {
+            suma = suma + tab[i];
+        }
+
+        double srednia = suma / tab.Length;
+        return srednia;
     }
 }
